eab3d2135d319d5b3e1cb6444a53ad26a7cc5342
Matthieu Fernandes <matthieu.fernandes@gmail.com>
2025-04-17 12:04:43 +0200

    fix(release.sh): fix script args parser

--- release.sh
+++ release.sh
@@ -20,6 +20,7 @@ release(){
     fi
     check_repo_ready_for_release
 
+    echo "Release ${project} to ${version}:"
     git checkout gh-pages
     git pull
     git merge master --no-edit
@@ -30,6 +31,7 @@ release(){
     git add "helm/${project}/${project}-${version}.tgz"
     git commit -m "Publish ${project} chart ${version}"
     git push origin gh-pages
+    echo "Release Done ✔ "
 }
 
 check_repo_ready_for_release(){
@@ -37,50 +39,35 @@ check_repo_ready_for_release(){
     then
         return 0
     fi
+    echo "Your git repo is not clean - please cleanup before release." >&2
     return 1
 }
 
 ### Parse args ###
-COMMAND=""
+COMMAND="help"
 COMMANDS="$(sed -n '/^[a-z].*(){$/ s/(){//p' $0)"
 project=""
 version=""
 
-while [[ $# -gt 0 ]]
-do
-    key="$1"
-    shift
-    case $key in
-        --project)
-            project="$2"
-            shift
-        ;;
-        --version)
-            version="$2"
-            shift
-        ;;
-        -h|--help|help)
-            help
-            exit 0
-        ;;
+# === Argument Parser ===
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --project) project="$2"; shift 2;;
+        --version) version="$2"; shift 2;;
+        -h|--help|help) COMMAND="help"; shift; break;;
         *)
-            if echo "${COMMANDS}" | grep -q "${key}"
-            then
-                COMMAND="${key}"
-            else
-                echo "unknown arg : ${key}" >&2
-                exit 1
-            fi
+          if echo "${1}" | grep -q "${COMMANDS}"
+          then
+            COMMAND="${1}"
+          else
+            echo "unknown arg : ${1}" >&2
+            exit 1
+          fi
+          shift
         ;;
     esac
 done
 
 ### main ###
-if [[ -z "${COMMAND}" ]]
-then
-    help
-    exit 0
-fi
-
 "${COMMAND}"
 
